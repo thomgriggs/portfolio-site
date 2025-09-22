76e82deca55513e48fad87a4a8260e4e79b0d09c
Thom Griggs <thomgriggs@gmail.com>
2025-09-22 12:08:03 -0400

    fix: export ProjectCard as default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { ProjectCard, ProjectCardSkeleton } from './ProjectCard';
+export { default as ProjectCard, ProjectCardSkeleton } from './ProjectCard';
 export { default as Hero } from './Hero';
